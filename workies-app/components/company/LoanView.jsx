bf81175d9ecd1bebe877820d9655ccbcac60e4b2
dylviz <dylanvizcarra@gmail.com>
2022-08-21 05:51:55 -0700

    add table to the company page

---
+++ workies-app/components/company/LoanView.jsx
@@ -0,0 +1,28 @@
+
+import { Box, Container, Heading, Table, Thead, Tr, Th, Tbody } from "@chakra-ui/react";
+import EmplListItem from "../employee/EmplListItem";
+
+export function LoanView(props){
+
+    return (
+        <Box p={5}>
+            <Heading>Loan Requests</Heading>
+            <Box overflowY="auto" maxHeight="1000px" minW={"1000"}>
+                <Table variant="striped" colorScheme="facebook">
+                    <Thead position="sticky" bgColor="grey">
+                        <Tr>
+                            <Th color={"blackAlpha.900"}>Loan</Th>
+                            <Th color={"blackAlpha.900"}>Amount Paid</Th>
+                            <Th color={"blackAlpha.900"}>Due Date</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {props.data.map((lender, idx) => (
+                            <EmplListItem lender={lender} key={idx}/>
+                        ))}
+                    </Tbody>
+                </Table>
+            </Box>
+        </Box>
+    );
+}
--- workies-app/pages/company/index.js
+++ workies-app/pages/company/index.js
@@ -1,9 +1,21 @@
 import { Box, Text, VStack, Flex, HStack } from "@chakra-ui/react";
 
-import RegisterCompanyForm from "../../components/company/RegisterCompanyForm"
+import RegisterCompanyForm from "../../components/company/RegisterCompanyForm";
+import { LoanView } from "../../components/company/LoanView";
 
 export default function employee() {
+    const dummyData = [
+        { loan: "5000", amountPaid: "1000", dueDate: "082022" },
+        { loan: "10000", amountPaid: "3000", dueDate: "082022" },
+        { loan: "10000", amountPaid: "2000", dueDate: "082022" },
+        { loan: "5000", amountPaid: "1000", dueDate: "082022" },
+        { loan: "10000", amountPaid: "3000", dueDate: "082022" },
+        { loan: "10000", amountPaid: "2000", dueDate: "082022" },
+    ];
     return (
-        <RegisterCompanyForm />
+        <VStack>
+            <RegisterCompanyForm />
+            <LoanView data={dummyData}/>
+        </VStack>
     );
 }

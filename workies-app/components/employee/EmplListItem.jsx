6015e7ac7d0534c95fac8dff9aa0312b068383f9
nadia <vanesalinas7325@gmail.com>
2022-08-21 08:23:41 -0500

    m

--- workies-app/components/employee/EmplListItem.jsx
+++ workies-app/components/employee/EmplListItem.jsx
@@ -1,12 +1,13 @@
 import { Box, Button, Td, Tr } from "@chakra-ui/react";
-
+import {utils} from  "ethers";
 export default function EmplListItem(props){
+    console.log(props.lender.amount)
     return(
-
         <Tr>
-            <Td>{props.lender.loan}</Td>
-            <Td>{props.lender.loan}</Td>
-            <Td>{props.lender.loan}</Td>
+            <Td>{props.lender.borrower[0]}</Td>
+            <Td>{utils.formatEther(props.lender.amount[0])}</Td>
+            <Td  onClick={props.lender.lend()} >Approve</Td>
+            
         </Tr>
     )
 }
